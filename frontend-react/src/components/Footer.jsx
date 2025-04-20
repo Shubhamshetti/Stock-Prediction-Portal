6044327d0b0c06950b2fb28388c42d7c912b4182
shubhamshetti <shettishubham7@gmail.com>
2025-04-20 10:02:59 +0530

    homepage react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className="footer py-3 my-3">
+    <hr className='border-bottom'/>
+    <p className='text-center text-light'>© 2025 Stock Prediction Portal. All rights reserved. Build with ❤️ by Shubham Shetti</p>
+
+  
+
+    </div>
+    </>
+  )
+}
+
+export default Footer
